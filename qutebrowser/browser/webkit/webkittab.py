0a504f44fe127b0251ff8a33f9d9dca8de71e825
Rakan Alhneiti <rakan.alhneiti@gmail.com>
2019-10-29 16:44:00 +0100

    Group lazy loading functionality into AbstractHistory

--- qutebrowser/browser/webkit/webkittab.py
+++ qutebrowser/browser/webkit/webkittab.py
@@ -751,7 +751,7 @@ class WebKitTab(browsertab.AbstractTab):
         self._widget.load(url)
 
     def url(self, *, requested=False):
-        if not self.loaded and self.history.to_load:
+        if not self.history.loaded and self.history.to_load:
             idx = self.history.current_idx()
             return self.history.to_load[idx].url
 
@@ -780,7 +780,7 @@ class WebKitTab(browsertab.AbstractTab):
         return self._widget.icon()
 
     def reload(self, *, force=False):
-        if not self.loaded:
+        if not self.history.loaded:
             self.load()
             return
 
